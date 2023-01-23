61b851958f75e82752095934694cc78e172c03a2
AlenLong <alenlong1@gmail.com>
2023-01-23 10:09:20 -0300

    Stopwatch Timer 2 vel control

--- src/components/myPractices/StopwatchTimer2/index.jsx
+++ src/components/myPractices/StopwatchTimer2/index.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import { Button, ButtonGroup, Card, Col, Container, Row } from 'react-bootstrap'
 
+const oneOrTwoNum = (num) => num > 9 ? num : `0${num}`
+
 export const StopwatchTimer2 = () => {
   const [seconds, setSeconds] = useState(0)
   const [minutes, setMinutes] = useState(0)
   const [hours, setHours] = useState(0)
+
+  const [velocity, setVelocity] = useState(0)
+  const [velocityName, setVelocityName] = useState('')
 
   const [stateIntervalSeconds, setIntervalSeconds] = useState(null)
   const [stateIntervalMinutes, setIntervalMinutes] = useState(null)
@@ -13,16 +18,13 @@ export const StopwatchTimer2 = () => {
 
   const handleStart = () => {
 
-
-    const intervalSeconds = setInterval(() => {
-      setSeconds((sec) => {
-        if (sec === 59) {
-          return 0
-        }
-        return sec + 1
+    
+    const intervalHours = setInterval(() => {
+      setHours((hrs) => {
+        return hrs + 1
       })
-    }, 1000 / 1000);
-
+    }, velocity ? (60000 * 60) / velocity : (60000 * 60));
+    
     const intervalMinutes = setInterval(() => {
       setMinutes((min) => {
         if (min === 59) {
@@ -30,13 +32,21 @@ export const StopwatchTimer2 = () => {
         }
         return min + 1
       })
-    }, 61000 / 1000);
-
-    const intervalHours = setInterval(() => {
-      setHours((hrs) => {
-        return hrs + 1
+    },velocity ? 60000 / velocity : 60000) ;
+    
+    const intervalSeconds = setInterval(() => {
+      setSeconds((sec) => {
+        if (sec === 59) {
+          return 0
+        }
+        return sec + 1
       })
-    }, 61000 * 60 / 1000);
+    }, velocity ? 1000 / velocity : 1000);
+
+
+
+
+
 
     setIntervalSeconds(intervalSeconds)
     setIntervalMinutes(intervalMinutes)
@@ -55,6 +65,19 @@ export const StopwatchTimer2 = () => {
     setIntervalHours(null)
   }
 
+  const handleReset = () => {
+    handleStop();
+    setSeconds(0);
+    setMinutes(0);
+    setHours(0);
+  }
+
+  const handleVelocity = (vel, velText) =>{
+    setVelocity(vel)
+    setVelocityName(velText)
+    handleStop()
+    /* handleStart() */
+  }
 
 
   return (
@@ -62,7 +85,7 @@ export const StopwatchTimer2 = () => {
       <Row className="mt-5">
         <Col xs={12} lg={{ span: 6, offset: 3 }} className={'text-center'}>
 
-          <ButtonGroup aria-label="Basic example">
+          <ButtonGroup aria-label="Basic example" className='d-block my-2'>
 
             <Button variant="outline-success"
               disabled={stateIntervalSeconds &&
@@ -71,17 +94,27 @@ export const StopwatchTimer2 = () => {
               onClick={handleStart}>Comenzar
             </Button>
             <Button variant="outline-danger" onClick={handleStop}>Detener</Button>
-            <Button variant="outline-secondary">Reiniciar</Button>
+            <Button variant="outline-secondary" onClick={handleReset}>Reiniciar</Button>
           </ButtonGroup>
+
+          <ButtonGroup aria-label="Basic example">
+
+            <Button variant="outline-info" className={velocityName ==='min' && 'active'} onClick={()=>handleVelocity(0,'min')}>min</Button>
+            <Button variant="outline-info" className={velocityName ==='x2' && 'active'} onClick={()=>handleVelocity(2, 'x2')}>x2</Button>
+            <Button variant="outline-info" className={velocityName ==='x4' && 'active'} onClick={()=>handleVelocity(4, 'x4')}>x4</Button>
+            <Button variant="outline-info" className={velocityName ==='x6' && 'active'} onClick={()=>handleVelocity(6, 'x6')}>x6</Button>
+            <Button variant="outline-info" className={velocityName ==='max' && 'active'} onClick={()=>handleVelocity(10, 'max')}>max</Button>
+          </ButtonGroup>
+
 
           <Card style={{ width: '25rem' }} className="m-auto my-3">
             <Card.Body>
               <Card.Title>Stop watch Timer 2</Card.Title>
               <Card.Text>
-                {hours} horas | {minutes} minutos | {seconds} segundos
+                {oneOrTwoNum(hours)} horas | {oneOrTwoNum(minutes)} minutos | {oneOrTwoNum(seconds)} segundos
               </Card.Text>
             </Card.Body>
-            <Card.Footer>{hours}:{minutes}:{seconds}</Card.Footer>
+            <Card.Footer>{oneOrTwoNum(hours)}:{oneOrTwoNum(minutes)}:{oneOrTwoNum(seconds)}</Card.Footer>
           </Card>
 
         </Col>
